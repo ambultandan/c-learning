40e2cfcfacb33938a8aa75f9c3c17ef1eabb09da
Ambul <ambultandan@gmail.com>
2026-09-10 23:28:26 +0530

    conditional statements

--- main.cpp
+++ main.cpp
@@ -3,9 +3,41 @@
 using namespace std;
 
 int main(){
-    string name = "Golu";
+    int day = 5;
 
-    cout << "Hello, \'" << name << "\'" << "\n";
-
+    switch(day){
+        case 1: {
+            cout << "Monday\n";
+            break;
+        }
+        case 2: {
+            cout << "Tuesday\n";
+            break;
+        }
+        case 3: {
+            cout << "Wednesday\n";
+            break;
+        }
+        case 4: {
+            cout << "Thursdays\n";
+            break;
+        }
+        case 5: {
+            cout << "Friday\n";
+            break;
+        }
+        case 6: {
+            cout << "Saturday\n";
+            break;
+        }
+        case 7: {
+            cout << "Sunday\n";
+            break;
+        }
+        default : {
+            cout << "Invalid Input\n";
+            break;
+        }
+    }
     return 0;
 }
---
+++ practices/Q11.cpp
@@ -0,0 +1,13 @@
+// Take character as input and check whether it is a vowel or consonant(using switch).
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char chara;
+    cout << "Enter a character: ";
+    cin >> chara;
+
+    
+    return 0;
+}
