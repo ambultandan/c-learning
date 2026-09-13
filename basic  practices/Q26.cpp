7dd9a3fe4dc745c70d6a2446cd33e3f00421d9fc
Ambul <ambultandan@gmail.com>
2026-09-13 23:00:36 +0530

    just one while loop questionTwT

---
+++ basic  practices/Q26.cpp
@@ -0,0 +1,17 @@
+// TAke a number N as i nput and calculate the sum of its digit.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cout << "Enter the number:";
+    cin >> n;
+    while(n!=0){
+        int LastDigit = n%10;
+        sum += LastDigit;
+        n = n/10;
+    }
+    cout << "Sum = " << sum << "\n ";
+    return 0;
+}
